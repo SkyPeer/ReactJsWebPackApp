34fb6160e67c3ee1f01829922d84f70fbc6f524b
Oleg <o.selivancev@gmail.com>
2018-10-11 02:26:17 +0300

    ReactJsWebpackUpdate

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
 import TaskList from './tasklist'
-
+import './index2'
 
 
 ReactDOM.render(<TaskList/>, document.getElementById('index'));
+
