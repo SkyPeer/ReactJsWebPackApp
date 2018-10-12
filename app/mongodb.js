3ef497402a607314cb744a5e14c9d4e301b7c69e
Oleg <o.selivancev@gmail.com>
2018-10-12 21:39:24 +0300

    mongodb add

--- app/mongodb.js
+++ app/mongodb.js
@@ -1,9 +1,11 @@
 const mongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/";
 
-
+let data = '';
 
     mongoClient.connect(url, function (err, client) {
+
+
 
         const db = client.db("usersdb");
         const collection = db.collection("users");
@@ -11,15 +13,22 @@ const url = "mongodb://localhost:27017/";
         if (err) return console.log(err);
 
 
-        let getData = collection.find().toArray(function (err, results) {
+        collection.find().toArray(function (err, results) {
             client.close();
-            console.log('results', results);
+          //  console.log('results', results);
+            data = results;
             return results;
         });
+
     });
 
-
 function func() {
+    console.log('------------- data: ',data);
+}
+setTimeout(func, 1500);
+
+
+/*function func() {
     console.log( 'SetTimeOut Worked' );
 }
-setTimeout(func, 1000);
+setTimeout(func, 100);*/
